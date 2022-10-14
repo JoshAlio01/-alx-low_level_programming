4da6ad47d87dafe43548fd6cc7b249f3254cd0cc
JoshAlio01 <joshuaalimikhena@gmail.com>
2022-10-13 21:26:18 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
+ * main - Prints sized of characters
  *
- *  * main - Prints sized of characters
- *
- *   *
- *
- *    *Return: Always 0 (success)
- *
+ *Return: Always 0 (success)
  */
 int main(void)
 {
